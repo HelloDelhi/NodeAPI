8ee621bdc48c901de2a38fdc66eee3422471d361
HelloRahul33 <rahulsurajmal@gmail.com>
2020-08-12 12:35:21 +0530

    my changes

--- server/fetch/fetch.js
+++ server/fetch/fetch.js
@@ -1,6 +1,6 @@
 let mongodb= require('mongodb')
 let naresh=  mongodb.MongoClient
-let MongoDB_URI='mongodb://HelloRahul:JqkkLjhSggDAuw9v@clustor0.1hlyz.mongodb.net/online?retryWrites=true&w=majority';
+let MongoDB_URI='mongodb+srv://HelloRahul:JqkkLjhSggDAuw9v@clustor0.1hlyz.mongodb.net/online?retryWrites=true&w=majority';
 
 let fetch= require('express').Router().get('/',(req,res)=>{
      naresh.connect(MongoDB_URI,(err,db)=>{
